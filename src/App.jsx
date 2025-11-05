94e111301fc01435123c97fdbf68028f31367e9f
1OmarTarek1 <cr7omartarek@gmail.com>
2025-11-05 19:04:19 +0200

    remove unused

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
 import { Navbar, Footer } from "./layouts";
 import { AppRoutes } from "./routes";
 import AOS from 'aos';
@@ -7,30 +6,23 @@ import 'aos/dist/aos.css';
 import { ScrollToTop, ToTopBtn } from './Components';
 import './App.css';
 
-
 const App = () => {
-  const { i18n } = useTranslation();
-  
   useEffect(() => {
     AOS.init({
-      duration: 800, // مدة الانيميشن بالملي ثانية
+      duration: 800,
     });
+    
+    // Set RTL for Arabic
+    document.body.dir = 'rtl';
+    document.body.className = 'rtl';
+    document.documentElement.lang = 'ar';
+    document.documentElement.dir = 'rtl';
+    document.body.style.fontFamily = 'Tajawal, sans-serif';
   }, []);
-
-  useEffect(() => {
-    // اتجاه النص
-    document.body.dir = i18n.dir();
-
-    // class rtl/ltr
-    document.body.className = i18n.language === 'ar' ? 'rtl' : 'ltr';
-
-    // تحديد اللغة على الـ <html>
-    document.documentElement.lang = i18n.language;
-  }, [i18n.language]);
 
   return (
     <>
-      <div className={`app ${i18n.language === 'ar' ? 'rtl' : 'ltr'}`}>
+      <div className="app rtl">
         <Navbar />
         <main className="main-content">
           <AppRoutes />
--- src/layouts/navbar/navbar.jsx
+++ src/layouts/navbar/navbar.jsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { IoCallOutline, IoHomeOutline, IoImagesOutline } from "react-icons/io5";
-import { FiUsers } from "react-icons/fi";
-import LanguageToggleBtn from '../../Components/LanguageToggleBtn/LanguageToggleBtn';
+import { IoHomeOutline, IoImagesOutline } from "react-icons/io5";
 import HamburgerBtn from '../../Components/HamburgerBtn/HamburgerBtn';
 import ThemeToggle from "../../Components/ThemeToggle/ThemeToggle";
 import logo from "../../Assets/Images/Logo/logo2.png";
@@ -12,7 +9,6 @@ import './navbar.css';
 import { GrDocumentPerformance } from 'react-icons/gr';
 
 const Navbar = () => {
-  const { t } = useTranslation();
   const [isOpen, setIsOpen] = useState(false);
   const [show, setShow] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
@@ -58,7 +54,7 @@ const Navbar = () => {
         <NavLink to="/" className="navLogo">
           <img src={logo} alt="Logo" className="logoImg" />
           <span className="nayalText">
-            {t("app.title", { defaultValue: "Curtains Shop" })}
+            متجر الستائر
             <br />
             <span className="nayalTextSubtitle">خصوصية اناقة و شياكة</span>
           </span>
@@ -69,25 +65,19 @@ const Navbar = () => {
           <li className="navItem">
             <NavLink to="/" end className="navLink" onClick={() => setIsOpen(false)}>
               <span className="linkIcn"><IoHomeOutline /></span>
-              <span>{t("navbar.home")}</span>
-            </NavLink>
-          </li>
-          <li className="navItem">
-            <NavLink to="/AboutPage" className="navLink" onClick={() => setIsOpen(false)}>
-              <span className="linkIcn"><FiUsers /></span>
-              <span>{t("navbar.about")}</span>
+              <span>الرئيسية</span>
             </NavLink>
           </li>
           <li className="navItem">
             <NavLink to="/GalleryPage" className="navLink" onClick={() => setIsOpen(false)}>
               <span className="linkIcn"><IoImagesOutline /></span>
-              <span>{t("navbar.products", { defaultValue: "Products" })}</span>
+              <span>منتجاتنا</span>
             </NavLink>
           </li>
           <li className="navItem">
             <NavLink to="/InventoryPage" className="navLink" onClick={() => setIsOpen(false)}>
               <span className="linkIcn"><AiOutlineShop size={21}/></span>
-              <span>{t("navbar.inventory")}</span>
+              <span>المخزون</span>
             </NavLink>
           </li>
           <li className="navItem">
@@ -95,13 +85,7 @@ const Navbar = () => {
               <span className="linkIcn">
                 <GrDocumentPerformance size={18}/>
               </span>
-              <span>{t("navbar.sales", { defaultValue: "Sales" })}</span>
-            </NavLink>
-          </li>
-          <li className="navItem">
-            <NavLink to="/ContactPage" className="navLink" onClick={() => setIsOpen(false)}>
-              <span className="linkIcn"><IoCallOutline /></span>
-              <span>{t("navbar.contact")}</span>
+              <span>المبيعات</span>
             </NavLink>
           </li>
         </ul>
@@ -109,7 +93,6 @@ const Navbar = () => {
         {/* Right side */}
         <div className="navActions">
           <ThemeToggle />
-          <LanguageToggleBtn />
           <div className="hamburger">
             <HamburgerBtn 
               isOpen={isOpen} 
