d16aa74cf24fc501b33cd9549628f274757f8a08
1OmarTarek1 <cr7omartarek@gmail.com>
2025-11-05 19:10:35 +0200

    remove unused

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import { HomePage, AboutUsPage, GalleryPage, ContactPage, InventoryPage, SalesPage } from "../Pages";
+import { HomePage, GalleryPage, InventoryPage, SalesPage } from "../Pages";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/AboutPage" element={<AboutUsPage />} />
       <Route path="/GalleryPage" element={<GalleryPage />} />
       <Route path="/InventoryPage" element={<InventoryPage />} />
       <Route path="/Sales" element={<SalesPage />} />
-      <Route path="/ContactPage" element={<ContactPage />} />
       <Route path="*" element={<HomePage />} />
     </Routes>
   );
