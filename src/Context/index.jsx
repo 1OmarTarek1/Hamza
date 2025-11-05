3798728e29d61e1dc0e11b3aa83dfce35ec32ee2
1OmarTarek1 <cr7omartarek@gmail.com>
2025-11-05 19:07:33 +0200

    remove unused

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,7 +1,5 @@
 // Re-export all context providers from this file
-export * from './LanguageContext/LanguageContext';
 export * from './ThemeContext';
 
 // Export the default providers for backward compatibility
-export { default as LanguageProvider } from './LanguageContext/LanguageContext';
 export { ThemeProvider } from './ThemeContext';
